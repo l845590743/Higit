1a5ddee9fde28c1a9fc3cffc09d45842ce0e4a88
zhimin.liu <845590743@qq.com>
2016-07-30 13:11:42 +0800

    first commit sqldb file
    Signed-off-by: zhimin.liu <845590743@qq.com>

---
+++ sqldb/src/main/java/com/example/sqldb/User.java
@@ -0,0 +1,12 @@
+package com.example.sqldb;
+
+public class User {
+	public String name;
+	public int age;
+	public String phone;
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + ", phone=" + phone + "]";
+	}
+	
+}
